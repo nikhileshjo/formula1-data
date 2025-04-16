b42a33a8f4cc747dc16defb2bc0d23d959a9e156
Vyangesh <joshisonu252@gmail.com>
2025-04-16 20:39:04 +0530

    ADD: install_missing_modules.py RENAMe: api_dumps.py to schedule_api_dumps.py ADD: pythonDependencies.txt ADD: init/SQL/Schedule created schema for schedule

---
+++ init/SQL/Schedule/date_dim.sql
@@ -0,0 +1,10 @@
+CREATE TABLE date_dim (
+    date_id INT PRIMARY KEY,
+    full_timestamp TIMESTAMP,
+    local_timestamp TIMESTAMP,
+    day_of_week VARCHAR(20),
+    month_name VARCHAR(20),
+    year INT,
+    quarter INT CHECK (quarter >= 1 AND quarter <= 4),
+    is_night_race BOOLEAN
+);
---
+++ init/SQL/Schedule/event_dim.sql
@@ -0,0 +1,6 @@
+CREATE TABLE event_dim (
+    event_id INT PRIMARY KEY,
+    official_event_name VARCHAR(300),
+    event_name VARCHAR(100),
+    event_type varchar(100)
+);
